f572ef79ead06ce3fa3db7771455ba746333700e
tissadeking <34912974+tissadeking@users.noreply.github.com>
2022-10-08 10:11:16 +0200

    Update further_setup_others.sh

--- further_setup_others.sh
+++ further_setup_others.sh
@@ -1,41 +1,31 @@
 #!/bin/sh
 
-minikube kubectl -- get po -A
+minikube start -driver=docker
 
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+export TIMEOUT=2592000s
 
-sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+arkade install openfaas \
 
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+  --set gateway.upstreamTimeout=$TIMEOUT \
 
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  --set gateway.writeTimeout=$TIMEOUT \
 
-kubectl version --client
+  --set gateway.readTimeout=$TIMEOUT \
 
-kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.4
+  --set faasnetes.writeTimeout=$TIMEOUT \
 
-kubectl expose deployment hello-minikube --type=NodePort --port=8080
+  --set faasnetes.readTimeout=$TIMEOUT \
 
-kubectl get services hello-minikube
-
-minikube service hello-minikube
-
-curl -sL https://cli.openfaas.com | sudo sh
-
-curl -SLsf https://dl.get-arkade.dev/ | sudo sh
-
-arkade install openfaas
+  --set queueWorker.ackWait=$TIMEOUT
 
 kubectl rollout status -n openfaas deploy/gateway
 
 kubectl port-forward -n openfaas svc/gateway 8080:8080 &
 
-kubectl -n openfaas get deployments -l "release=openfaas, app=openfaas"
-
-kubectl get svc -o wide gateway-external -n openfaas
+kubectl get deployments -n openfaas -l "release=openfaas, app=openfaas"
 
 PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
 
-echo -n $PASSWORD | faas-cli login --username admin -password-stdin
+echo -n $PASSWORD | faas-cli login --username admin --password-stdin
 
 echo $PASSWORD
