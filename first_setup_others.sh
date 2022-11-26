0b29f20cea9b3335e474250f8189a9342c47437d
tissadeking <34912974+tissadeking@users.noreply.github.com>
2022-11-26 14:55:41 +0100

    Update first_setup_others.sh

--- first_setup_others.sh
+++ first_setup_others.sh
@@ -2,23 +2,9 @@
 
 minikube kubectl -- get po -A
 
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
-
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+snap install kubectl --classic
 
 kubectl version --client
-
-kubectl create deployment hello-minikube --image=k8s.gcr.io/echoserver:1.4
-
-kubectl expose deployment hello-minikube --type=NodePort --port=8080
-
-kubectl get services hello-minikube
-
-minikube service hello-minikube
 
 curl -sL https://cli.openfaas.com | sudo sh
 
